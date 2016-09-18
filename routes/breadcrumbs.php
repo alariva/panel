69e1d9e4bd8004c1c801226a19f13445928ef6b4
Ariel Vallese <alariva@gmail.com>
2016-09-18 18:27:16 -0300

    Fix breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,18 +1,18 @@
 <?php
 
 // Home
-Breadcrumbs::register('home', function ($breadcrumbs) {
+Breadcrumbs::register('/', function ($breadcrumbs) {
     $breadcrumbs->push(trans('nav.home'), route('home'));
 });
 
 // Dashboard
 Breadcrumbs::register('dashboard', function ($breadcrumbs) {
-    $breadcrumbs->parent('home');
+    $breadcrumbs->parent('/');
     $breadcrumbs->push(trans('nav.dashboard'), route('frontoffice.dashboard.show'));
 });
 
 // Terms and Conditions
 Breadcrumbs::register('terms-and-conditions', function ($breadcrumbs) {
-    $breadcrumbs->parent('home');
+    $breadcrumbs->parent('/');
     $breadcrumbs->push(trans('nav.terms-and-conditions'), route('frontoffice.terms-and-conditions.show'));
 });
